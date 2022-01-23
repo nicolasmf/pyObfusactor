31d3a46302cb162f453a7d20002c6df4de8a5d89
nicolasmf <nikolamf@hotmail.com>
2022-01-24 00:02:33 +0100

    Added findVariableNames

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from ast import Return
+from keyword import iskeyword
 import re
 
 def findFunctionNames(file):
 	function_names = []
 	for _, line in enumerate(open(file)):
-		for name in re.findall(r"(?<=def ).+(?=\()", line):
+		for name in re.findall(r"(?<=def )\w+(?=\()", line):
 			function_names.append(name)
 	return function_names
 
+def findVariableNames(file):
+	variable_names = []
+	for _, line in enumerate(open(file)):
+		regex = r"((\w+)(?=( ||\t)=( |)))|((?<=\()\w+(?=\)))|((?<=\()\w+(?=,))|((?<=, )\w+(?=,))|((?<=, )\w+(?=\)))|(\w+(?=, ))"
+		for name in re.findall(regex, line):
+			variable_names.append(list(filter((lambda x: x!='' and x!=' '), name))[0])
+	return list(set(variable_names))
+
 if __name__ == "__main__":
-	print(findFunctionNames("test.py"))
+	print("Variables : " + str(findVariableNames("test.py")))
+	print("Functions : " + str(findFunctionNames("test.py")))
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-def bar():
-	pass
+def bar(name):
+	print(name)
 
 def foo(x, y, z):
 	print("This is definitely not a function.")
+
+var1, variable1 = 1
+
+variable2=2
